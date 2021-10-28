ab455535b16fab763e61e714538a09bd033aff1c
Alexander Staeding <alexanderstaeding@gmail.com>
2021-10-28 12:27:03 +0200

    Keep consistent ordering of build script elements

--- grader/build.gradle.kts
+++ grader/build.gradle.kts
@@ -1,5 +1,4 @@
 dependencies {
   implementation(project(":solution"))
   implementation("org.sourcegrade:jagr-grader-api:0.1.0-SNAPSHOT")
-  implementation("org.sourcegrade:fopbot:0.1.0-SNAPSHOT")
 }
